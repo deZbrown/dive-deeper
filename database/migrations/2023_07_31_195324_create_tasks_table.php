3774f58988dbc1db6840e124b89b9c3f394416a5
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-07-31 22:45:27 +0200

    Created task migration.

--- database/migrations/2023_07_31_195324_create_tasks_table.php
+++ database/migrations/2023_07_31_195324_create_tasks_table.php
@@ -11,8 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('tasks', static function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable()->default(null);
+            $table->integer('pomodoro')->nullable()->default(null);
+            $table->boolean('completed')->default(false);
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('project_id')->constrained();
+            $table->integer('date')->nullable()->default(null);
             $table->timestamps();
         });
     }
